341817b9e963231a10198403cf21ad004be904cf
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2023-04-12 07:36:57 +0800

    wip laporan form a

--- app/Exports/LaporanFormAExport.php
+++ app/Exports/LaporanFormAExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Opd;
+use App\Models\SubKegiatan;
 use Carbon\Carbon;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
@@ -21,9 +22,9 @@ class LaporanFormAExport implements FromView
 
     public function view(): View
     {
-        $bulan = Carbon::createFromFormat('Y-m-d', $this->bulan);
-        $bulanLaluMulai = $bulan->subMonth(1)->startOfMonth->toDateString();
-        $bulanLaluSelesai = $bulan->subMonth(1)->endOfMonth->toDateString();
+        $bulan = Carbon::createFromFormat('Y-m-d', '2024-06-06');
+        $bulanLaluMulai = $bulan->subMonth(1)->startOfMonth()->toDateString();
+        $bulanLaluSelesai = $bulan->subMonth(1)->endOfMonth()->toDateString();
 
         $bulanIniMulai = $bulan->startOfMonth()->toDateString();
         $bulanIniSelesai = $bulan->endOfMonth()->toDateString();
@@ -31,16 +32,19 @@ class LaporanFormAExport implements FromView
         $sdBulanIniMulai = $bulan->startOfYear()->toDateString();
         $sdBulanIniSelesai = $bulan->endOfMonth()->toDateString();
 
-        $opds = Opd::query()
-            ->join('sub_opds AS so', 'so.opd_id', '=', 'opds.id')
-            ->join('bidang_urusan_sub_opds AS buso', 'buso.sub_opd_id', '=', 'so.id')
-            ->join('objek_realisasis AS or', 'or.bidang_urusan_sub_opd_id', '=', 'buso.id')
-            ->join('realisasis AS r', 'or.id', '=', 'r.objek_realisasi_id')
-            ->join('sub_kegiatans AS sk', 'or.sub_kegiatan_id', '=', 'sk.id')
-            ->join('kegiatans AS k', 'sk.kegiatan_id', '=', 'k.id')
+        $opds = SubKegiatan::query()
+            ->join('kegiatans AS k', 'sub_kegiatans.kegiatan_id', '=', 'k.id')
             ->join('programs AS p', 'k.program_id', '=', 'p.id')
+            ->join('objek_realisasis AS or', 'or.sub_kegiatan_id', '=', 'sub_kegiatans.id')
+            ->join('realisasis AS r', 'or.id', '=', 'r.objek_realisasi_id')
+            ->join('bidang_urusan_sub_opds AS buso', 'buso.id', '=', 'or.bidang_urusan_sub_opd_id')
+            ->join('sub_opds AS so', 'so.id', '=', 'buso.sub_opd_id')
+            ->join('opds', 'opds.id', '=', 'so.opd_id')
 
-            ->selectRaw("p.nama AS nama_program, k.nama AS nama_kegiatan, sk.nama AS nama_sub_kegiatan, or.anggaran, SUM(IF(r.tanggal BETWEEN {$bulanLaluMulai} AND {$bulanLaluSelesai}, r.jumlah, 0)) AS realisasi_bulan_lalu, SUM(IF(r.tanggal BETWEEN {$bulanIniMulai} AND {$bulanIniSelesai}, r.jumlah, 0)) AS realisasi_bulan_ini, SUM(IF(r.tanggal BETWEEN {$sdBulanIniMulai} AND {$sdBulanIniSelesai}, r.jumlah, 0)) AS realisasi_sd_bulan_ini, opds.id AS opd_id, so.id AS sub_opd_id, or.tahapan_apbd_id")
+            ->selectRaw("p.nama AS nama_program, k.nama AS nama_kegiatan, sub_kegiatans.nama AS nama_sub_kegiatan, or.anggaran, SUM(IF(r.tanggal BETWEEN {$bulanLaluMulai} AND {$bulanLaluSelesai}, r.jumlah, 0)) AS realisasi_bulan_lalu, SUM(IF(r.tanggal BETWEEN {$bulanIniMulai} AND {$bulanIniSelesai}, r.jumlah, 0)) AS realisasi_bulan_ini, SUM(IF(r.tanggal BETWEEN {$sdBulanIniMulai} AND {$sdBulanIniSelesai}, r.jumlah, 0)) AS realisasi_sd_bulan_ini, opds.id AS opd_id, so.id AS sub_opd_id, or.tahapan_apbd_id, or.bidang_urusan_sub_opd_id, or.sub_kegiatan_id, or.sub_rincian_objek_belanja_id, p.id AS program_id, k.id AS kegiatan_id")
+            ->groupBy('sub_kegiatans.nama, k.nama, p.nama, or.anggaran, opds.id, so.id, or.tahapan_apbd_id,
+            or.bidang_urusan_sub_opd_id, or.sub_kegiatan_id, or.sub_rincian_objek_belanja_id')
+            ->dd()
             ->get();
 
         return view('exports.laporan-form-a-export', compact('opds'));
--- resources/views/exports/laporan-form-a-export.blade.php
+++ resources/views/exports/laporan-form-a-export.blade.php
@@ -1,176 +1,206 @@
 <table>
-  <tbody>
-    <tr>
-      <td colspan="14">FORM. A</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td colspan="14">LAPORAN BULANAN PELAKSANAAN PROGRAM/KEGIATAN/SUBKEGIATAN</td>
-    </tr>
-    <tr>
-      <td colspan="14">DOKUMEN PELAKSANAAN ANGGARAN APBD PROVINSI NTB</td>
-    </tr>
-    <tr>
-      <td colspan="14">TAHUN ANGGARAN...</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>Nama Urusan</td>
-      <td>:</td>
-      <td>...............</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>Organisasi</td>
-      <td>:</td>
-      <td>...............</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>Periode</td>
-      <td>:</td>
-      <td>Bulan ...</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td rowspan="4">No</td>
-      <td rowspan="4">Nama Program/Kegiatan/Subkegiatan</td>
-      <td rowspan="4">Jumlah Anggaran (Rp)</td>
-      <td rowspan="4">Bobot (%)</td>
-      <td colspan="9">Realisasi Pelaksanaan Anggaran</td>
-      <td rowspan="4">Sisa Anggaran (Rp)</td>
-    </tr>
-    <tr>
-      <td colspan="3">Bulan Lalu</td>
-      <td colspan="3">Bulan Ini</td>
-      <td colspan="3">s.d. Bulan Ini</td>
-    </tr>
-    <tr>
-      <td colspan="2">Keuangan</td>
-      <td rowspan="2">Fisik (%)</td>
-      <td colspan="2">Keuangan</td>
-      <td rowspan="2">Fisik (%)</td>
-      <td colspan="2">Keuangan</td>
-      <td rowspan="2">Fisik (%)</td>
-    </tr>
-    <tr>
-      <td>Rp.</td>
-      <td>%</td>
-      <td>Rp.</td>
-      <td>%</td>
-      <td>Rp.</td>
-      <td>%</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>2</td>
-      <td>3</td>
-      <td>4</td>
-      <td>5</td>
-      <td>6</td>
-      <td>7</td>
-      <td>8</td>
-      <td>9</td>
-      <td>10</td>
-      <td>11</td>
-      <td>12</td>
-      <td>13</td>
-      <td>14</td>
-    </tr>
-    @php
-      $program = null;
-      $kegiatan = null;
-      $subKegiatan = null;
-      $i = 1;
-    @endphp
-    @foreach ($opds as $opd)
+    <tbody>
+        <tr>
+            <td colspan="14">FORM. A</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="14">LAPORAN BULANAN PELAKSANAAN PROGRAM/KEGIATAN/SUBKEGIATAN</td>
+        </tr>
+        <tr>
+            <td colspan="14">DOKUMEN PELAKSANAAN ANGGARAN APBD PROVINSI NTB</td>
+        </tr>
+        <tr>
+            <td colspan="14">TAHUN ANGGARAN...</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Nama Urusan</td>
+            <td>:</td>
+            <td>...............</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Organisasi</td>
+            <td>:</td>
+            <td>...............</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>Periode</td>
+            <td>:</td>
+            <td>Bulan ...</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td rowspan="4">No</td>
+            <td rowspan="4">Nama Program/Kegiatan/Subkegiatan</td>
+            <td rowspan="4">Jumlah Anggaran (Rp)</td>
+            <td rowspan="4">Bobot (%)</td>
+            <td colspan="9">Realisasi Pelaksanaan Anggaran</td>
+            <td rowspan="4">Sisa Anggaran (Rp)</td>
+        </tr>
+        <tr>
+            <td colspan="3">Bulan Lalu</td>
+            <td colspan="3">Bulan Ini</td>
+            <td colspan="3">s.d. Bulan Ini</td>
+        </tr>
+        <tr>
+            <td colspan="2">Keuangan</td>
+            <td rowspan="2">Fisik (%)</td>
+            <td colspan="2">Keuangan</td>
+            <td rowspan="2">Fisik (%)</td>
+            <td colspan="2">Keuangan</td>
+            <td rowspan="2">Fisik (%)</td>
+        </tr>
+        <tr>
+            <td>Rp.</td>
+            <td>%</td>
+            <td>Rp.</td>
+            <td>%</td>
+            <td>Rp.</td>
+            <td>%</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>10</td>
+            <td>11</td>
+            <td>12</td>
+            <td>13</td>
+            <td>14</td>
+        </tr>
+        @php
+            $program = null;
+            $kegiatan = null;
+            $subKegiatan = null;
+            $i = 1;
+        @endphp
+        @foreach ($opds as $opd)
+			@php
+				$queryProgram = $opds->where('tahapan_apbd_id', cache('tahapanApbd')->id)
+					->where('bidang_urusan_sub_opd_id', $opd->bidang_urusan_sub_opd_id)
+					->where('program_id', $opd->program_id)
+					->where('sub_rincian_objek_belanja_id', $opd->sub_rincian_objek_belanja_id);
 
-    <tr>
-      <td>{{ $i++ }}</td>
-	  @if ($program != $opd->nama_program)
-		  <td>{{ $opd->nama_program }}</td>
-		  <td>{{ $opds->where('tahapan_apbd_id', cache('tahapanApbd')->id)->where('sub_opd_id') }}</td>
-		  @php
-			  $program = $opd->nama_program;
-		  @endphp
-	  @endif
-    </tr>
-    @endforeach
-  </tbody>
-  </table>
+				$queryKegiatan = $opds->where('tahapan_apbd_id', cache('tahapanApbd')->id)
+					->where('bidang_urusan_sub_opd_id', $opd->bidang_urusan_sub_opd_id)
+					->where('kegiatan_id', $opd->kegiatan_id)
+					->where('sub_rincian_objek_belanja_id', $opd->sub_rincian_objek_belanja_id);
+			@endphp
+            <tr>
+                <td>{{ $i++ }}</td>
+                @if ($program != $opd->nama_program)
+                    <td>{{ $opd->nama_program }}</td>
+                    <td>{{ $queryProgram->sum('anggaran') }}</td>
+                    <td>Bobot Program</td>
+					@php
+						$realisasiBulanLalu = $queryProgram->sum('realisasi_bulan_lalu');
+						$realisasiBulanIni = $queryProgram->sum('realisasi_bulan_ini');
+						$realisasiSdBulanIni = $queryProgram->sum('realisasi_sd_bulan_ini');
+						$anggaran = $queryProgram->sum('anggaran');
+					@endphp
+                    <td>{{ $realisasiBulanLalu }}</td>
+                    <td>{{ ($realisasiBulanLalu / $anggaran) * 100 }}</td>
+                    <td>FISIK</td>
+
+                    <td>{{ $realisasiBulanIni }}</td>
+                    <td>{{ ($realisasiBulanIni / $anggaran) * 100 }}</td>
+                    <td>FISIK</td>
+
+                    <td>{{ $realisasiSdBulanIni }}</td>
+                    <td>{{ ($realisasiSdBulanIni / $anggaran) * 100 }}</td>
+                    <td>FISIK</td>
+
+                    <td>{{ $anggaran - $realisasiSdBulanIni }}</td>
+                    @php
+                        $program = $opd->nama_program;
+                    @endphp
+                @endif
+            </tr>
+        @endforeach
+    </tbody>
+</table>
