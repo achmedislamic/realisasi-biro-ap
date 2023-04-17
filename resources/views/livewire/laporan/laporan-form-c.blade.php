654491b8e0f8d405206f816b4ce7af9ee99c2323
achmedislamic <achmedislamic@outlook.com>
2023-04-17 12:47:23 +0800

    wip lap c

---
+++ resources/views/livewire/laporan/laporan-form-c.blade.php
@@ -0,0 +1,9 @@
+<x-slot:header>
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Laporan Form C
+    </h2>
+</x-slot:header>
+
+<x-container>
+    <x-laporan.form :$opds :$subOpds :$urusans :$bidangUrusans jenis-laporan="c" />
+</x-container>
