a3b9ce8711b181854fd356a91192f2a3c731246f
achmedislamic <achmedislamic@outlook.com>
2023-04-30 15:28:13 +0800

    upgrade to laravel 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,8 +23,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
-
         Gate::define('ubah-realisasi', function (User $user) {
             if ($user->isAdmin()) {
                 return true;
