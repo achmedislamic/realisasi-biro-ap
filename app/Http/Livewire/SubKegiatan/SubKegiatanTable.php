b8680336ffa6e35f074a6bdb52f26da70ff022f5
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2023-05-16 15:36:08 +0800

    fix group by subkegiatan table

--- app/Http/Livewire/SubKegiatan/SubKegiatanTable.php
+++ app/Http/Livewire/SubKegiatan/SubKegiatanTable.php
@@ -56,13 +56,13 @@ class SubKegiatanTable extends Component
                     ->join('sub_opds AS so', 'buso.sub_opd_id', '=', 'so.id')
                     ->join('opds AS o', 'so.opd_id', '=', 'o.id')
                     ->when(filled($this->opdId), function ($query) {
-                        $query->where('o.id', $this->opdId)
-                            ->groupByRaw('so.id, sub_kegiatans.id');
-                    })->when(filled($this->subOpdId), function ($query) {
-                        $query->where('so.id', $this->subOpdId)
-                            ->groupByRaw('so.id, sub_kegiatans.id');
+                        $query->where('o.id', $this->opdId);
+                    })
+                    ->when(filled($this->subOpdId), function ($query) {
+                        $query->where('so.id', $this->subOpdId);
                     })
                     ->select('sub_kegiatans.id', 'sub_kegiatans.kode', 'sub_kegiatans.nama', 'o.kode AS kode_opd', 'o.nama AS nama_opd', 'so.kode AS kode_sub_opd', 'so.nama AS nama_sub_opd', 'o.id AS opd_id', 'so.id AS sub_opd_id')
+                    ->groupByRaw('so.id, sub_kegiatans.id')
                     ->orderBy('sub_kegiatans.kode')
                     ->orderBy('sub_kegiatans.nama');
             })
