8e1660ac8212683c8017dadf6b0f716e6a521605
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2023-10-03 09:39:13 +0800

    update background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,10 +53,10 @@
                 </div>
             </nav>
 
-            <div class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+            <div class="bg-center bg-no-repeat bg-[url('{!! asset('img/walpaper-hero.webp') !!}')] bg-gray-700 bg-cover">
                 <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                    <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Sistem Informasi Pembangunan</h1>
-                    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">...</p>
+                    {{-- <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Sistem Informasi Pembangunan</h1> --}}
+                    {{-- <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">...</p> --}}
                     <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     </div>
                 </div>
