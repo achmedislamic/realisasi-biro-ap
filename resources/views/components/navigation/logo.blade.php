db45265531eba70d38e08fc3e892f1978921c9d0
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2023-11-21 10:16:27 +0800

    update logo

--- resources/views/components/navigation/logo.blade.php
+++ resources/views/components/navigation/logo.blade.php
@@ -1,14 +1,10 @@
-<a href="#" class="flex items-start gap-2 group">
-    <div class="border border-utama p-2 rounded-md hover:text-white hover:bg-utama">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-        </svg>
+<a href="#" class="flex items-center gap-2 group">
+    <div class="p-2 rounded-md hover:text-white hover:bg-utama">
+        <img src="{{ asset('img/logo-sipbang.webp') }}" class="h-8 w-auto">
     </div>
     <p class="text-sm font-light uppercase">
-        Dashboard
         <span class="text-base block font-bold tracking-widest">
-            Realisasi Biro AP
+            Biro AP
         </span>
     </p>
 </a>
