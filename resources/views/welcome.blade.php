4be82aaa441ec83b104826c53547a17c6abbbdec
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2024-02-04 15:16:58 +0800

    change apps description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
             <div class="h-full w-full bg-cover bg-center bg-no-repeat bg-gray-400 bg-blend-multiply" style='background-image: url("{{ asset('img/wallpaper-realisasi.jpeg') }}");'>
                 <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
                     <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Sistem Informasi Pembangunan</h1>
-                    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">...</p>
+                    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Aplikasi Pengelolaan Realisasi Anggaran Dinas Pekerjaan Umum dan Penataan Ruang</p>
                     <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     </div>
                 </div>
